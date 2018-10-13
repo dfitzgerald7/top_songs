ad4502f416421660c4bd3c422a0f664869f153c6
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-10-13 17:05:12 -0400

    basic cli menu

--- lib/top_songs/cli.rb
+++ lib/top_songs/cli.rb
@@ -2,7 +2,16 @@ class TopSongs::CLI
 
 
   def run
-    puts "Now running CLI."
+      puts "Welcome to the top songs according to Billboard 100!"
+      menu
+  end
+
+  def menu
+    puts "Please enter a command:
+        Hot 100
+        Artist 100
+        Rock
+        Hip-Hop"
   end
 
 end
