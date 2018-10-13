7432391644fc947cc22c9024cad8b4c275f36101
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-10-13 15:42:57 -0400

    can run with ./bin/top_songs

--- lib/top_songs.rb
+++ lib/top_songs.rb
@@ -1,5 +1,15 @@
-require "top_songs/version"
 
+#require gems
+require "nokogiri"
+require "open-uri"
+require "pry"
+
+
+#use for name spacing
 module TopSongs
   # Your code goes here...
 end
+
+
+#Require files
+require "top_songs/version"
