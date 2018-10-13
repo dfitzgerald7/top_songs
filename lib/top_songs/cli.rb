68be989c02748423a9387c91f6d2ce7d886fc5af
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-10-13 18:36:49 -0400

    more cli

--- lib/top_songs/cli.rb
+++ lib/top_songs/cli.rb
@@ -12,14 +12,21 @@ class TopSongs::CLI
         input = gets.chomp.downcase
         if input == "menu"
           menu
-        elsif input == "hot 100"
-          TopSongs::Charts.find_or_create("Hot 100")
-        elsif input == "artist 100"
-          TopSongs::Charts.find_or_create("Artist 100")
-        elsif input == "rock"
-          TopSongs::Charts.find_or_create("Rock")
-        elsif input == "hip-hop" || "hip hop"
-          TopSongs::Charts.find_or_create("Hip-Hop")
+        # elsif input == "hot 100"
+        #   TopSongs::Charts.find_or_create("Hot 100")
+        # elsif input == "artist 100"
+        #   TopSongs::Charts.find_or_create("Artist 100")
+        # elsif input == "rock"
+        #   TopSongs::Charts.find_or_create("Rock")
+        # elsif input == "hip-hop" || "hip hop"
+        #   TopSongs::Charts.find_or_create("Hip-Hop")
+
+      elsif ["hot 100", "artist 100", "rock", "Hip-Hop", "Hip Hop"].include?(input)
+          TopSongs::Charts.find_or_create(input)
+          #----------------WANTS
+          #Another screen that says 'would you like to see the top 100 list?'
+          #uses the charts object to grab url and print ordered list of songs
+
         else
           puts "Invalid input. Enter 'Menu' to see commands"
         end
