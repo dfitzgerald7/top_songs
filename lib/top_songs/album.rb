72984b0e571633dd638b16ecd8ab7d69dd82b272
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-10-15 12:04:18 -0400

    song and album class

---
+++ lib/top_songs/album.rb
@@ -0,0 +1,6 @@
+
+class TopSongs::Album
+  attr_accessor :artist, :title
+
+
+end
---
+++ lib/top_songs/song.rb
@@ -0,0 +1,13 @@
+
+
+class TopSongs::Song
+  attr_accessor :artists, :title
+
+
+  def print_list
+    puts self.chart_name
+    song_titles.each.with_index(1) {|song, index| puts "#{index}. #{song} by #{artists[index - 1]} "}
+  end
+
+
+end
